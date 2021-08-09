60edca5a677e860687c71aa33de25da2299c8f26
korsar <pavel.isupov@mail.ru>
2021-08-09 16:19:49 +0300

    Прикрутил карту в карточку клиента (view)

--- application/views/customers/view.php
+++ application/views/customers/view.php
@@ -4,8 +4,43 @@
 //
 //echo $customer_item['name'] . ' ' . $customer_item['surname'] . ' ' . $customer_item['middle_name']; ?>
 
-<?php foreach ($customer_item as $value): ?>
+<div class="row">
+	<div class="col-lg-12 margin-tb">
+		<div class="pull-right">
+			<p><a class="btn btn-success" href="<?php echo base_url('customers/edit/'.$customer_item['id']) ?>"> Редактировать</a></p>
+		</div>
+	</div>
+</div>
 
-	<p><?php echo $value ;?></p>
+<div class="row">
+	<div class="col">
+		<?php foreach ($customer_item as $value): ?>
 
-<?php endforeach; ?>
+			<p><?php echo $value ;?></p>
+
+		<?php endforeach; ?>
+	</div>
+	<div class="col" id="map" style="width: 400px;height: 400px">
+
+	</div>
+
+</div>
+
+
+<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+
+<script type="text/javascript">
+	ymaps.ready(init);
+	function init(){
+		var myMap = new ymaps.Map("map", {
+			center: [56.039017, 92.894853],
+			zoom: 16
+		});
+
+		var marker = new ymaps.Placemark([56.039017, 92.894853], {
+			hintContent: 'Расположение',
+			balloonContent: 'Это наша организация'
+		});
+		myMap.geoObjects.add(marker);
+	}
+</script>
